ee4fada9605f99959fcb6ed317bccac5bcac525e
avoiddeveloper <karimwdd03@gmail.com>
2024-11-24 17:41:42 +0600

    Guessing game logic emplimentation

---
+++ app.js
@@ -0,0 +1,15 @@
+let won = 0;
+let lose = 0;
+for (let i = 1; i <= 5; i++) {
+    const input = parseInt(prompt("Enter A Number 1 to 5"));
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    if (input === randomNumber) {
+        console.log("Your Won " + "The Number Was " + randomNumber);
+        won++;
+    } else {
+        console.log("Your Lose " + "The Number Was " + randomNumber);
+        lose++;
+    }
+}
+document.write("You Won " + won + " Times <br>");
+document.write("You Lose " + lose + " Times");
